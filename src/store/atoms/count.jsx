f14005bb8e2dfe9932aaa737bb77d23ee8a5b63a
SRINJOY DAS <115316528+srinju@users.noreply.github.com>
2024-07-26 20:43:51 +0530

    Update count.jsx

--- src/store/atoms/count.jsx
+++ src/store/atoms/count.jsx
@@ -1,7 +1,16 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export const countAtom = atom({
     key: "countAtom", //unique key for each atom 
     default : 0 //default value of the state 
 });
 
+//defining my recoil selector >
+
+export const evenSelector = selector({
+    key : "evenSelector",
+    get: ({get}) => {
+        const count = get(countAtom); //we are passsing count atom here because the evenselector depends on the count state so we will put the state or atom in this case of those which are dependent 
+        return count % 2;
+    }
+});
